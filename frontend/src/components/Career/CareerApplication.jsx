135cfe6f22a4e6c99fe550a6c6564133b4568f0f
Ajay7616 <u.ajaykumar7616@gmail.com>
2026-08-26 17:21:55 +0530

    Deployment issue resolved

--- frontend/src/components/Career/CareerApplication.jsx
+++ frontend/src/components/Career/CareerApplication.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { submitCareerApplication } from "../../api/api";
 
 function CareerApplication() {
